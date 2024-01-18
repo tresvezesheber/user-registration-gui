758a5f270dd7a48555159110d1dd1d8db1af265d
desenv2 <webmaster@universidadedevassouras.edu.br>
2024-01-18 12:49:02 -0300

    Table of Users Inserted in Users Page

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
-
+  users = [
+    {
+      id: 1,
+      name: 'Astrofoboldo Silveira',
+      email: 'astro@astro.il',
+      password: 'Husu&55SHJkaka1_00a',
+    },
+    {
+      id: 2,
+      name: 'Guilhermina da Silva',
+      email: 'mina@mina.br',
+      password: '(88UJHsao!oa&6ggTt3',
+    },
+    {
+      id: 3,
+      name: 'Giuseppe Caccini',
+      email: 'giucacci@giucacci.it',
+      password: 'aII8ÇL&hbbTtA433',
+    },
+  ];
 }
